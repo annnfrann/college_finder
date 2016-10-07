4551fd63ed1236e4d56fa8e4557b7d1ecf7191d1
Anna Lewis <anna.frances.lewis@gmail.com>
2016-10-07 11:48:16 -0600

    api routes dynamic

--- routes/api.js
+++ routes/api.js
@@ -18,34 +18,64 @@ router.get('/names', function(req, res, next){
     res.json(college_names)
   })
 })
-router.get('/states', function(req, res, next){
-  Queries.states().then(function(college_states){
-    res.json(college_states)
-  })
-})
-router.get('/enrollment', function(req, res, next){
-  Queries.enrollment().then(function(enrollment){
-    res.json(enrollment)
-  })
-})
+// router.get('/states', function(req, res, next){
+//   Queries.states().then(function(college_states){
+//     res.json(college_states)
+//   })
+// })
+// router.get('/enrollment', function(req, res, next){
+//   Queries.enrollment().then(function(enrollment){
+//     res.json(enrollment)
+//   })
+// })
 router.get('/admissions', function(req, res, next){
   Queries.admissions().then(function(admissions){
     res.json(admissions)
   })
 })
-router.get('/mensSports', function(req, res, next){
-  Queries.mensSports().then(function(mensSports){
-    res.json(mensSports)
+// router.get('/mensSports', function(req, res, next){
+//   Queries.mensSports().then(function(mensSports){
+//     res.json(mensSports)
+//   })
+// })
+// router.get('/womensSports', function(req, res, next){
+//   Queries.womensSports().then(function(womensSports){
+//     res.json(womensSports)
+//   })
+// })
+router.get('/sports/:gender', function(req, res, next){
+  Queries.getSports(req.params.gender).then(function(genderSports){
+    res.json(genderSports)
   })
 })
-router.get('/womensSports', function(req, res, next){
-  Queries.womensSports().then(function(womensSports){
-    res.json(womensSports)
+// router.get('/division', function(req, res, next){
+//   Queries.division().then(function(division){
+//     res.json(division)
+//   })
+// })
+router.get('/division/:gender/:sport', function(req, res, next){
+  Queries.getDivision(req.params.gender, req.params.sport).then(function(divisions){
+    res.json(divisions)
   })
 })
-router.get('/division', function(req, res, next){
-  Queries.division().then(function(division){
-    res.json(division)
+router.get('/enrollment/:sportId', function(req, res, next){
+  Queries.getEnrollment(req.params.sportId).then(function(enrollments){
+    res.json(enrollments)
+  })
+})
+router.get('/selectivity/:lowerEnrollment/:upperEnrollment/:sportId', function(req, res, next){
+  Queries.getSelectivity(req.params.lowerEnrollment, req.params.upperEnrollment, req.params.sportId).then(function(selectivity){
+    res.json(selectivity)
+  })
+})
+router.get('/location/:lowerEnrollment/:upperEnrollment/:sportId/:selectivity', function(req, res, next){
+  Queries.getLocation(req.params.lowerEnrollment, req.params.upperEnrollment, req.params.sportId, req.params.selectivity).then(function(locations){
+    res.json(locations)
+  })
+})
+router.get('/results/:lowerEnrollment/:upperEnrollment/:sportId/:selectivity/:state', function(req, res, next){
+  Queries.getResults(req.params.lowerEnrollment, req.params.upperEnrollment, req.params.sportId, req.params.selectivity, req.params.state).then(function(results){
+    res.json(results)
   })
 })
 router.get('/test', function(req, res, next){
