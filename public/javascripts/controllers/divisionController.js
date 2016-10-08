7968312bdeac38c26a99dd33d445504f5eba5ab4
Anna Lewis <anna.frances.lewis@gmail.com>
2016-10-08 15:55:36 -0600

    updating controllers and factory

--- public/javascripts/controllers/divisionController.js
+++ public/javascripts/controllers/divisionController.js
@@ -1,7 +1,14 @@
 app.controller('divisionController', function($scope, colleges_service){
   $scope.view = {}
 
-  colleges_service.division().then(function(division){
-    $scope.view.division = division.data.rows
+  // colleges_service.division().then(function(division){
+  //   $scope.view.division = division.data.rows
+  // })
+  colleges_service.getDivision(colleges_service.studentParameters.gender, colleges_service.studentParameters.sportName).then(function(divisions){
+    $scope.view.divisions = divisions.data
   })
+  $scope.view.chosenDivision = ''
+  $scope.getDivision = function(input){
+    colleges_service.studentParameters.sportId = input
+  }
 })
--- public/javascripts/controllers/sportController.js
+++ public/javascripts/controllers/sportController.js
@@ -3,4 +3,8 @@ app.controller('sportController', function($scope, colleges_service){
   colleges_service.getSports(colleges_service.studentParameters.gender).then(function(sports){
     $scope.view.sports = sports.data
   })
+  $scope.view.sport = ''
+  $scope.getSport = function(input){
+    colleges_service.studentParameters.sportName = input
+  }
 })
