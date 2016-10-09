4a08861751baf6c9ea18c975bbb43823da8e69d5
Anna Lewis <anna.frances.lewis@gmail.com>
2016-10-09 14:34:41 -0600

    enrollment ng-show functional

--- public/javascripts/controllers/enrollmentController.js
+++ public/javascripts/controllers/enrollmentController.js
@@ -1,7 +1,5 @@
 app.controller('enrollmentController', function($scope, colleges_service){
   $scope.view = {}
-  $scope.view.lowerEnrollment = ''
-  $scope.view.upperEnrollment = ''
 
   $scope.getEnrollment = function(lower, upper){
     colleges_service.studentParameters.lowerEnrollment = lower
