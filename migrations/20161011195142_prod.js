d695a0b189b1d3caab1741d25041d38d078fa2b2
Anna Lewis <anna.frances.lewis@gmail.com>
2016-10-11 22:04:48 -0600

    small change. looking for a bug

--- migrations/20161011195142_prod.js
+++ migrations/20161011195142_prod.js
@@ -25,9 +25,9 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.dropTable('colleges'),
-        knex.schema.dropTable('sports'),
-        knex.schema.dropTable('college_sports')
-    ])
+  return Promise.all([
+      knex.schema.dropTable('colleges'),
+      knex.schema.dropTable('sports'),
+      knex.schema.dropTable('college_sports')
+  ])
 };
