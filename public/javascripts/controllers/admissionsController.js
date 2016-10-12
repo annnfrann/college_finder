d41f8a078fd5b31b245efb5eaf0fa9e158dfff5f
Anna Lewis <anna.frances.lewis@gmail.com>
2016-10-12 09:54:19 -0600

    last commit

--- public/javascripts/controllers/admissionsController.js
+++ public/javascripts/controllers/admissionsController.js
@@ -2,6 +2,7 @@ app.controller('admissionsController', function($scope, colleges_service){
   $scope.view = {}
 
   colleges_service.getSelectivity(colleges_service.studentParameters.lowerEnrollment, colleges_service.studentParameters.upperEnrollment, colleges_service.studentParameters.sportId).then(function(selectivity){
+    console.log(selectivity.data);
     $scope.view.selectivities = selectivity.data
   })
 
